dd71bf832f805bd30f7b2c97b96f12361aa74e35
panxibei <panxibei@yandex.com>
2019-01-28 16:00:05 +0800

    xzsoft

--- resources/views/admin/layouts/adminbase.blade.php
+++ resources/views/admin/layouts/adminbase.blade.php
@@ -230,7 +230,7 @@
 			<div><br><br><br><br></div>
 			<Layout :style="{padding: '0 12px 24px', marginLeft: '200px'}">
 				<!--内容主体-->
-				<Content :style="{padding: '24px 12px', minHeight: '280px', background: '#fff'}">
+				<Content :style="{padding: '0px 12px', minHeight: '280px', background: '#fff'}">
 				<!-- 主体 -->
 				@section('my_body')
 				@show
